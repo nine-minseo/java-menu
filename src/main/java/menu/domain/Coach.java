4e3c33069c0d1f6c28173e5c54f96e02e7323a50
구민서 <minseo12379@gmail.com>
2026-01-09 22:18:33 +0900

    feat: 요일별로 각 코치가 먹을 메뉴 추천하는 기능 구현

--- src/main/java/menu/domain/Coach.java
+++ src/main/java/menu/domain/Coach.java
@@ -1,13 +1,37 @@
 package menu.domain;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Coach {
     private String name;
     private List<String> isInedibleMenu;
+    private List<String> fixedMenu;
 
-    public Coach(String name, List<String> isInedibleMenu) {
+    public Coach(String name) {
         this.name = name;
-        this.isInedibleMenu = isInedibleMenu;
+        isInedibleMenu = new ArrayList<>();
+        fixedMenu = new ArrayList<>();
+    }
+
+    public void setIsInedibleMenu(List<String> isInedibleMenu) {
+        this.isInedibleMenu = new ArrayList<>(isInedibleMenu);
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public List<String> getIsInedibleMenu() {
+        return Collections.unmodifiableList(isInedibleMenu);
+    }
+
+    public List<String> getFixedMenu() {
+        return Collections.unmodifiableList(fixedMenu);
+    }
+
+    public void add(String menu) {
+        fixedMenu.add(menu);
     }
 }
