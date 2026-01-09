4bf89c49e2b9be1d222f4fca89a54f98151d75ae
구민서 <minseo12379@gmail.com>
2026-01-09 17:07:24 +0900

    feat: 요일별로 주어진 카테고리를 랜덤으로 정하는 기능 구현

--- src/main/java/menu/Application.java
+++ src/main/java/menu/Application.java
@@ -1,7 +1,8 @@
 package menu;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import menu.domain.Coach;
 import menu.view.InputView;
 import menu.view.OutputView;
@@ -19,6 +20,24 @@ public class Application {
             Coach coach = new Coach(name, isInedibleMenu);
         }
 
+        List<String> categories = new ArrayList<>(List.of("일식", "한식", "중식", "아시안", "양식"));
+        List<String> categoriesByDay = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            while (true) {
+                String category = categories.get(Randoms.pickNumberInRange(0, 4));
+                System.out.println(category);
+                int categorieCount = (int) categoriesByDay.stream()
+                        .filter(ctg -> ctg.equals(category))
+                        .count();
+
+                if (categorieCount < 2) {
+                    categoriesByDay.add(category);
+                    break;
+                }
+            }
+        }
+
+        System.out.println(categoriesByDay);
     }
 
     private static <T> T retryUntilValid(Supplier<T> supplier) {
